77b347b11aa2601a1a16b499844379e1a01c709f
Arunesh Mathur <aruneshmathur1990@gmail.com>
2012-04-22 16:41:32 +0530

    SICP 2.35,2.36,2.37

--- accumalator.scm
+++ accumalator.scm
@@ -28,3 +28,42 @@
 		(+ (* 2 higher-terms) this-coeff))
 	       0
 	       coefficient-sequence))
+
+
+; SICP 2.35
+
+(define (count-leaves t)
+  (accumalator + 0 (map (lambda (node)
+			  (if (pair? node)
+			    (count-leaves node)
+			    1))
+			t)))
+
+; SICP 2.36
+
+(define (accumalate-n op init seqs)
+  (if (null? (car seqs))
+    '()
+    (cons (accumalator op 
+		       init 
+		       (map car seqs))
+	  (accumalate-n op init (map cdr seqs)))))
+
+; SICP 2.37
+
+(define (dot-product u v)
+  (accumalator + 0 (map * u v)))
+
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product x v)) m))
+
+
+(define (transpose mat)
+  (accumalate-n cons '() mat))
+
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x))
+	 m)))
